b7a6448ca8c42550493a3e3e13b5ee5817e03fea
kelvin <34615778+kelvinwtan@users.noreply.github.com>
2020-02-17 19:09:59 +1100

    installed next.js

---
+++ .next/static/webpack/static/development/pages/index.js.1456eacdab4b0d61641d.hot-update.js
@@ -0,0 +1,37 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./src/pages/index.tsx":
+/*!*****************************!*\
+  !*** ./src/pages/index.tsx ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Home; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/kelvintan/KelvinTan/club-sandwich/src/pages/index.tsx";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+function Home() {
+  return __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }, "Hello Sandwichy"));
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.1456eacdab4b0d61641d.hot-update.js.map
